1b8e682be8fcc9a794af0b9da377e8d91ba46bd8
Phong Tran <aphtr001@edu.xamk.fi>
2020-05-18 16:18:56 +0700

    added event handler

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { randomBytes } from 'crypto';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
 
@@ -14,15 +15,29 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'Comment Created',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  console.log('Received event: ', event.type);
+  res.end();
 });
 
 app.listen(4001, () => {
--- events/index.js
+++ events/index.js
@@ -5,3 +5,15 @@ import axios from 'axios';
 const app = express();
 
 app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  res.send({ status: 'ok' });
+});
+
+app.listen(4005, () => {
+  console.log('Listening on: 4005');
+});

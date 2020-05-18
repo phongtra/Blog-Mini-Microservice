017ceb13131bb299782c911d20cbf07cc505035f
Phong Tran <aphtr001@edu.xamk.fi>
2020-05-18 16:32:35 +0700

    save post and comment to query service

--- query/index.js
+++ query/index.js
@@ -6,9 +6,24 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/events', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'Post Created') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  } else if (type === 'Comment Created') {
+    const { id, content, postId } = data;
+    posts[postId].comments.push({ id, content });
+  }
+  console.log(posts);
+  res.end();
+});
 
 app.listen(4002, () => {
   console.log('Listening on: 4002');
